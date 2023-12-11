bdcf9507bcca23743d475eb9e027fad3c78c654c
LeonardoMelloSilvestri <leonardomscontato@gmail.com>
2023-12-11 20:41:29 -0300

    Adding React Router to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./components/Home";
+import Statistics from "./components/Statistics";
+import Summary from "./components/Summary";
+import Chronology from "./components/Chronology";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <Home />
+            <Statistics />
+            <Summary />
+          </>
+        ),
+      },
+      {
+        path: "cronologia",
+        element: <Chronology />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
